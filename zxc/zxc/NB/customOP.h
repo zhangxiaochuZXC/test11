59c54229e0e7b59f5af4de9ed6330a5a742ef071
ITManager <zhangjiesharp@163.com>
2016-08-19 23:01:52 +0800

    收徒弟 哈哈哈对方的回复后

---
+++ zxc/zxc/NB/customOP.h
@@ -0,0 +1,13 @@
+//
+//  customOP.h
+//  zxc
+//
+//  Created by zhangjie on 16/8/19.
+//  Copyright © 2016年 zhangjie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface customOP : NSOperation
+
+@end
---
+++ zxc/zxc/NB/customOP.m
@@ -0,0 +1,13 @@
+//
+//  customOP.m
+//  zxc
+//
+//  Created by zhangjie on 16/8/19.
+//  Copyright © 2016年 zhangjie. All rights reserved.
+//
+
+#import "customOP.h"
+
+@implementation customOP
+
+@end
