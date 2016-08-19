b41bf9c50bdc891afd67552378cb3c1028a783e3
ITManager <zhangjiesharp@163.com>
2016-08-19 22:56:21 +0800

    创建项目

---
+++ zxc/zxc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zxc
+//
+//  Created by zhangjie on 16/8/19.
+//  Copyright © 2016年 zhangjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zxc/zxc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  zxc
+//
+//  Created by zhangjie on 16/8/19.
+//  Copyright © 2016年 zhangjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
